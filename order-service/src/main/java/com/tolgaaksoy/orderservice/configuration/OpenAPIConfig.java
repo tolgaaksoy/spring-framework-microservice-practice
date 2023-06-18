a2b8a50ecee597097aba43ca024f9ada7ceb69b7
Tolga Aksoy <dev.tolgaaksoy@gmail.com>
2023-06-19 01:27:15 +0300

    Add(project): springdoc-openapi config class for documentation

---
+++ order-service/src/main/java/com/tolgaaksoy/orderservice/configuration/OpenAPIConfig.java
@@ -0,0 +1,16 @@
+package com.tolgaaksoy.orderservice.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("ORDER-SERVICE").version("v1"));
+    }
+
+}
---
+++ product-service/src/main/java/com/tolgaaksoy/productservice/configuration/OpenAPIConfig.java
@@ -0,0 +1,16 @@
+package com.tolgaaksoy.productservice.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("PRODUCT-SERVICE").version("v1"));
+    }
+
+}
