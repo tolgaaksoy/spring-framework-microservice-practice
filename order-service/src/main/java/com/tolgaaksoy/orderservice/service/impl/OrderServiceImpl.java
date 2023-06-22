c43c2598d57494b1599979ba2b8049121c9e105c
Tolga Aksoy <dev.tolgaaksoy@gmail.com>
2023-06-23 00:14:25 +0300

    Add(order-service): webClient basic logic

--- order-service/src/main/java/com/tolgaaksoy/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/tolgaaksoy/orderservice/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tolgaaksoy.orderservice.service.impl;
 
+import com.tolgaaksoy.orderservice.model.dto.OrderLineItemDto;
 import com.tolgaaksoy.orderservice.model.dto.request.OrderRequest;
 import com.tolgaaksoy.orderservice.model.entity.Order;
 import com.tolgaaksoy.orderservice.model.entity.OrderLineItem;
@@ -7,7 +8,10 @@ import com.tolgaaksoy.orderservice.repository.OrderRepository;
 import com.tolgaaksoy.orderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,10 +19,11 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, WebClient webClient) {
         this.orderRepository = orderRepository;
+        this.webClient = webClient;
     }
 
     @Override
@@ -26,15 +31,36 @@ public class OrderServiceImpl implements OrderService {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-        orderRequest.getOrderLineItemList().forEach(orderLineItemDto ->
-                order.getOrderLineItemList().add(OrderLineItem.builder()
-                        .skuCode(orderLineItemDto.getSkuCode())
-                        .quantity(orderLineItemDto.getQuantity())
-                        .price(orderLineItemDto.getPrice())
-                        .build()));
+        List<OrderLineItem> orderLineItemList = new ArrayList<>(orderRequest.getOrderLineItemList().size());
+
+        for (OrderLineItemDto orderLineItemDto : orderRequest.getOrderLineItemList()) {
+            if (!checkStock(orderLineItemDto)) {
+                log.warn("Insufficient Stock");
+                continue;
+                //throw new RuntimeException("Insufficient Stock");
+            }
+            OrderLineItem orderLineItem = new OrderLineItem();
+            orderLineItem.setQuantity(orderLineItemDto.getQuantity());
+            orderLineItem.setSkuCode(orderLineItemDto.getSkuCode());
+            orderLineItemList.add(orderLineItem);
+        }
+
+        order.setOrderLineItemList(orderLineItemList);
 
         orderRepository.save(order);
 
         log.info("Order Placed Successfully");
+    }
+
+    private boolean checkStock(OrderLineItemDto orderLineItemDto) {
+        Integer stock = webClient.get()
+                .uri("http://localhost:8082/api/inventory/quantitu/{skuCode}", orderLineItemDto.getSkuCode())
+                .retrieve()
+                .bodyToMono(Integer.class)
+                .block();
+        if (stock == null) {
+            return false;
+        }
+        return stock > 0;
     }
 }
